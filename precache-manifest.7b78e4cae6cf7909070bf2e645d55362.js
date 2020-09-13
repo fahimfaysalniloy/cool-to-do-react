d93ae5882d663c92453a8ff01152a124370ce732
fahim faysal niloy <fahimfaysal.buetbd@gmail.com>
2020-09-13 13:31:36 +0600

    Updates

---
+++ precache-manifest.7b78e4cae6cf7909070bf2e645d55362.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd47f8c44ac44f75cc9cb7d3a3cc7cd3",
+    "url": "//github.com/fahimfaysalniloy/react-first-project/index.html"
+  },
+  {
+    "revision": "9a3ea99bc16ac4b1a00e",
+    "url": "//github.com/fahimfaysalniloy/react-first-project/static/css/main.d6e90e39.chunk.css"
+  },
+  {
+    "revision": "91e89da0c999af19f2b4",
+    "url": "//github.com/fahimfaysalniloy/react-first-project/static/js/2.b3c74d16.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "//github.com/fahimfaysalniloy/react-first-project/static/js/2.b3c74d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a3ea99bc16ac4b1a00e",
+    "url": "//github.com/fahimfaysalniloy/react-first-project/static/js/main.78e2ef6e.chunk.js"
+  },
+  {
+    "revision": "828ed6fe9a870baf0ba4",
+    "url": "//github.com/fahimfaysalniloy/react-first-project/static/js/runtime-main.e9f5cda4.js"
+  }
+]);
